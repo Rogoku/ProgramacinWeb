eb8226c54f4cb871c40bb8627114eb1cc8339039
unknown <alumnos@H41-26.upc.edu.pe>
2016-08-17 07:54:54 -0500

    Primera Version

---
+++ Sesion2POO/src/main/java/pe/edu/upc/Sesion2POO/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.Sesion2POO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
